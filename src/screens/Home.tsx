852435fd6c0f47b0dae9a9cc5a528dae14d638c2
Isaac Israel <email.iisrael@gmail.com>
2022-04-29 10:59:32 +0300

    refactoring HomeScreen with search product

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -14,8 +14,13 @@ import { Colors } from '../constants/Colors';
 import useAppSelector from '../hooks/useAppSelector';
 import { Product } from '../types/Products';
 import Touchable from '../components/button/Touchable';
+import SearchInput from '../components/input/SearchInput';
+import Title from '../components/text/Title';
 
 const styles = StyleSheet.create({
+  searchInput: {
+    marginVertical: Sizes.GUTTER,
+  },
   product: {
     paddingHorizontal: Sizes.GUTTER * 0.25,
     paddingVertical: Sizes.GUTTER,
@@ -29,14 +34,19 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     opacity: 0.3,
   },
+  emptyTitle: {
+    marginVertical: Sizes.GUTTER * 2,
+    textAlign: 'center',
+  },
 });
 
 const Home: Screen<'Home'> = ({ navigation }) => {
   const listHash = useAppSelector<Record<number, Product>>(({ products }) => products.list);
   const list = useMemo(() => _values(listHash), [listHash]);
-
   const nextPage = useAppSelector<number | undefined>(({ products }) => products.nextPage);
   const isFetching = useIsRequestLoading(productsActions.FETCH_PRODUCTS);
+  const isSearching = useIsRequestLoading(productsActions.SEARCH_PRODUCTS);
+
   const dispatch = useDispatch();
 
   const onEndReached = (): void => {
@@ -55,6 +65,10 @@ const Home: Screen<'Home'> = ({ navigation }) => {
     navigation.navigate('ProductDetails', { productId: id });
   };
 
+  const onSearch = (query: string): void => {
+    dispatch(productsReducer.searchProducts(query));
+  };
+
   const renderProduct: ListRenderItem<Product> = ({ item: product }) => (
     <Touchable style={styles.product} onPress={onProductPress(product.id)}>
       <Body style={styles.productTitle}>{product.title}</Body>
@@ -63,14 +77,19 @@ const Home: Screen<'Home'> = ({ navigation }) => {
 
   const renderLoading = (): React.ReactElement => <ActivityIndicator size="large" />;
 
+  const renderEmptyList = (): React.ReactElement => <Title style={styles.emptyTitle}>No Product Found</Title>;
+
   return (
     <Container title="Product List">
+      <SearchInput containerStyle={styles.searchInput} onSearch={onSearch} />
       <FlatList
         data={list}
         renderItem={renderProduct}
+        ListEmptyComponent={renderEmptyList}
         ItemSeparatorComponent={() => <View style={styles.separator} />}
         onEndReached={onEndReached}
         onEndReachedThreshold={0}
+        ListHeaderComponent={isSearching ? renderLoading : undefined}
         ListFooterComponent={isFetching ? renderLoading : undefined}
       />
     </Container>
